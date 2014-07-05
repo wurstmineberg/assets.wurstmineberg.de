9ace083be5f805a7e1b05552ac8cd8420dfb6349
Fenhl <fenhl@fenhl.net>
2014-07-05 07:31:06 +0000

    Add invited people and guests to people page

--- js/people.js
+++ js/people.js
@@ -1,36 +1,47 @@
-function display_people_data(people) {
+function displayPeopleData(people) {
+    var displayedStatuses = [
+        'founding',
+        'later',
+        'postfreeze',
+        'former',
+        'vetoed',
+        'invited',
+        'guest'
+    ];
+    var statusMapping = {
+        invited: 'guest',
+        vetoed: 'former'
+    };
     people.list.forEach(function(person) {
-        if (!person.status in ['founding', 'later', 'postfreeze', 'former', 'vetoed']) {
-            return;
+        if (person.status in displayedStatuses) {
+            var minecraft = '';
+            var status = person.status in statusMapping ? statusMapping[person.status] : person.status;
+            var $name = $('<td>', {'class': 'username'}).html($('<a>', {'href': '/people/' + person.id}).text(person.interfaceName));
+            if (person.minecraft && person.minecraft.toLowerCase() !== person.interfaceName.toLowerCase()) {
+                $name.append($('<p>', {'class': 'muted'}).text(person['minecraft']));
+            }
+            var $description;
+            if (person.description) {
+                $description = $('<td>', {'class': 'description'}).html(sanitized(person['description'], ['A', 'EM', 'S', 'SPAN']));
+            } else {
+                $description = $('<td>', {'class': 'description small muted'}).html('You can update your description using the command <code>!<a href="//wiki.wurstmineberg.de/Commands#People">People</a> ' + person.id + ' description &lt;value&gt;...</code>.');
+            }
+            var $tr = $('<tr>', {'id': person.id}).html($('<td>', {'class': 'people-avatar'}).html(person.html_ava(32)));
+            $tr.append($name);
+            $tr.append($description);
+            $('#loading-' + status + "-table").before($tr);
         }
-        var minecraft = '';
-        var status = person.status == 'vetoed' ? 'former' : person.status;
-        var $name = $('<td>', {'class': 'username'}).html($('<a>', {'href': '/people/' + person.id}).text(person.interfaceName));
-        if (person.minecraft && person.minecraft.toLowerCase() !== person.interfaceName.toLowerCase()) {
-            $name.append($('<p>', {'class': 'muted'}).text(person['minecraft']));
-        };
-        var $description;
-        if (!person.description) {
-            $description = $('<td>', {'class': 'description small muted'}).html('You can update your description using the command <code>!<a href="//wiki.wurstmineberg.de/Commands#People">People</a> ' + person.id + ' description &lt;value&gt;...</code>.');
-        } else {
-            $description = $('<td>', {'class': 'description'}).html(sanitized(person['description'], ['A', 'EM', 'S', 'SPAN']));
-        }
-        var $tr = $('<tr>', {'id': person.id}).html($('<td>', {'class': 'people-avatar'}).html(person.html_ava(32)));
-        $tr.append($name);
-        $tr.append($description);
-        $('#loading-' + status + "-table").before($tr);
     });
-    
     $('.loading').remove();
 };
 
-function load_people_data() {
+function loadPeopleData() {
     $.when(API.people()).done(function(people) {
-        display_people_data(people);
+        displayPeopleData(people);
     })
     .fail(function() {
         $('.loading').children('td').html('Error: could not load people.json');
     });
 };
 
-load_people_data();
+loadPeopleData();
