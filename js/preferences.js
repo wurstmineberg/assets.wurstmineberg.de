16cc40338c18c26a541f6f1e462647730c360f27
farthen <monkey@farthen.de>
2015-09-11 03:52:01 +0200

    Change js for the website

--- js/preferences.js
+++ js/preferences.js
@@ -1,3 +1,13 @@
 $(function(){
     $('.form-colorpicker').colorpicker();
 });
+
+$(function(){
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+  $('.nav-tabs a').click(function (e) {
+    $(this).tab('show');
+    window.location.hash = this.hash;
+  });
+});
