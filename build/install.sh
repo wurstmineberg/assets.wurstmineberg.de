2e1bf3c94ccd321988addac970e94b2f3d59066f
Fenhl <fenhl@fenhl.net>
2017-06-04 16:48:38 +0000

    Travis: Try running install script as wurstmineberg instead

--- build/install.sh
+++ build/install.sh
@@ -8,7 +8,4 @@ cd systemd-minecraft
 python setup.py install
 cd ..
 
-sudo mkdir -p /opt/wurstmineberg/home
-sudo useradd --home-dir=/opt/wurstmineberg/home wurstmineberg
-sudo chown -R wurstmineberg:wurstmineberg /opt/wurstmineberg
-sudo -u wurstmineberg python -m minecraft update
+python -m minecraft update
