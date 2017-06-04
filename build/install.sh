44db8ded90f9f6a3223a40a6458f98861395b1c2
Fenhl <fenhl@fenhl.net>
2017-06-04 16:32:30 +0000

    Create wurstmineberg user and update Minecraft on Travis CI

--- build/install.sh
+++ build/install.sh
@@ -7,3 +7,8 @@ git clone https://github.com/wurstmineberg/systemd-minecraft.git
 cd systemd-minecraft
 python setup.py install
 cd ..
+
+sudo mkdir -p /opt/wurstmineberg/home
+sudo useradd --home-dir=/opt/wurstmineberg/home wurstmineberg
+sudo chown -R wurstmineberg:wurstmineberg /opt/wurstmineberg
+sudo -u wurstmineberg minecraft update
