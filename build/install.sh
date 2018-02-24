e2ced5096e6fc5391c0d8ebc2673fa5b272e5ac8
Fenhl <fenhl@fenhl.net>
2018-02-24 17:45:56 +0000

    Try to fix install.sh

--- build/install.sh
+++ build/install.sh
@@ -7,7 +7,7 @@ export WURSTMINEBERG_CONFIG_DIR=build
 pip install Pillow
 git clone https://github.com/wurstmineberg/systemd-minecraft.git
 cd systemd-minecraft
-python setup.py install
+pip install .
 cd ..
 
 python -m minecraft update
