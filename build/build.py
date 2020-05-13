a08150fb57d34869932d486ce653a0f80545ec9b
Fenhl <fenhl@fenhl.net>
2020-05-13 18:55:37 +0000

    Build items.json from items-base.json

--- build/build.py
+++ build/build.py
@@ -1,32 +1,42 @@
 #! /usr/bin/env python3
 
 import json
-import minecraft
+#import minecraft
 import pathlib
 import shutil
 import zipfile
 
+def build_items():
+    with pathlib.Path('json/items-base.json').open() as items_base_f:
+        items = json.load(items_base_f)
+    return items
+
+def minecraft_version():
+    return '1.15.2' #TODO automate?
+
 if __name__ == '__main__':
     # unzip client
-    version = minecraft.World().version()
-    client_jar_path = minecraft.CONFIG['paths']['clientVersions'] / version / '{}.jar'.format(version)
+    #version = minecraft.World().version()
+    version = minecraft_version()
+    #client_jar_path = minecraft.CONFIG['paths']['clientVersions'] / version / f'{version}.jar'
+    client_jar_path = pathlib.Path(f'/opt/wurstmineberg/.minecraft/versions/{version}/{version}.jar')
     with zipfile.ZipFile(str(client_jar_path)) as client_jar:
         client_jar.extractall('build/clientjar')
     client = pathlib.Path('build/clientjar')
     # copy advancements
     if pathlib.Path('json/advancements').exists():
         shutil.rmtree('json/advancements')
-    shutil.copytree(str(client / 'assets' / 'minecraft' / 'advancements'), 'json/advancements')
+    shutil.copytree(str(client / 'data' / 'minecraft' / 'advancements'), 'json/advancements')
+    # build items
+    if pathlib.Path('json/items.json').exists():
+        pathlib.Path('json/items.json').unlink()
+    with pathlib.Path('json/items.json').open('w') as items_json_f:
+        json.dump(build_items(), items_json_f, indent=4, sort_keys=True)
+        print(file=items_json_f) # add trailing newline
     # copy lang
     if pathlib.Path('json/lang.json').exists():
         pathlib.Path('json/lang.json').unlink()
     lang = {}
-    with (client / 'assets' / 'minecraft' / 'lang' / 'en_us.lang').open() as lang_f:
-        for line in lang_f.read().splitlines():
-            if '=' in line:
-                translation_key, translated_text = line.split('=', 1)
-                lang[translation_key] = translated_text
-    with pathlib.Path('json/lang.json').open('w') as lang_json_f:
-        json.dump(lang, lang_json_f, indent=4, sort_keys=True)
+    shutil.copy2(client / 'assets' / 'minecraft' / 'lang' / 'en_us.json', 'json/lang.json')
     # remove client files
     shutil.rmtree('build/clientjar')
