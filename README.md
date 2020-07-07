8edd5feaadf3cf75698ac96d10fb423465ba215f
Fenhl <fenhl@fenhl.net>
2020-07-07 03:48:18 +0000

    Stub of new items.json built from minecraft-data

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 This repository contains the files hosted at [assets.wurstmineberg.de](https://assets.wurstmineberg.de/). These files are used by the [wurstmineberg.de](https://wurstmineberg.de/) websites and/or other infrastructure.
+
+# Building
+
+After cloning or updating this repository, some files must be built using:
+
+```
+$ python3 build/build.py
+```
+
+This currently requires [Fenhl's fork of the `minecraft_data` package](https://github.com/fenhl/python-minecraft-data).
--- build/build.py
+++ build/build.py
@@ -1,25 +1,83 @@
 #! /usr/bin/env python3
 
 import json
-#import minecraft
 import pathlib
+import platform
 import shutil
 import zipfile
 
+import more_itertools # PyPI: more-itertools
+import requests # PyPI: requests
+
+import minecraft_data # https://github.com/fenhl/python-minecraft-data
+
+def _download(url, local_filename=None): #FROM http://stackoverflow.com/a/16696317/667338
+    if local_filename is None:
+        local_filename = url.split('#')[0].split('?')[0].split('/')[-1]
+        if local_filename == '':
+            raise ValueError('no local filename specified')
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+        f.flush()
+
 def build_items():
-    with pathlib.Path('json/items-base.json').open() as items_base_f:
-        items = json.load(items_base_f)
-    return items
+    data = minecraft_data(minecraft_version())
+    all_items = {}
+    for item in data.items_list:
+        all_items[item['name']] = {
+            'name': item['displayName'],
+            #TODO solid
+            #TODO image
+            #TODO damagedImages
+            #TODO blockID
+            'itemID': item['id'],
+            #TODO damageValues
+            #TODO effects
+            #TODO tagPath
+            #TODO tagVariants
+            #TODO obtaining
+            #TODO dropsSelf
+            #TODO whenPlaced
+            #TODO creativeMenu
+            #TODO pickBlock
+            #TODO durability
+            'stackable': item['stackSize']
+            #TODO alwaysGlow
+        }
+    return {'minecraft': all_items}
+
+def client_versions_path():
+    if platform.node() == 'gharch':
+        return minecraft.CONFIG['paths']['clientVersions']
+    else:
+        return pathlib.Path('/opt/wurstmineberg/.minecraft/versions')
+
+def download_client(version, client_jar_path):
+    client_jar_path.parent.mkdir(exist_ok=True, parents=True)
+    # get version info
+    versions_json = requests.get('https://launchermeta.mojang.com/mc/game/version_manifest.json').json()
+    version_dict = more_itertools.one(filter(lambda version_dict: version_dict.get('id') == version, versions_json['versions']))
+    version_json = requests.get(version_dict['url']).json()
+    # get client jar
+    _download(version_json['downloads']['client']['url'], local_filename=client_jar_path)
 
 def minecraft_version():
-    return '1.15.2' #TODO automate?
+    if platform.node() == 'gharch':
+        import minecraft # https://github.com/wurstmineberg/systemd-minecraft
+
+        return minecraft.World().version()
+    else:
+        return '1.16.1' #TODO automate?
 
 if __name__ == '__main__':
     # unzip client
-    #version = minecraft.World().version()
     version = minecraft_version()
-    #client_jar_path = minecraft.CONFIG['paths']['clientVersions'] / version / f'{version}.jar'
-    client_jar_path = pathlib.Path(f'/opt/wurstmineberg/.minecraft/versions/{version}/{version}.jar')
+    client_jar_path = client_versions_path() / version / f'{version}.jar'
+    if not client_jar_path.exists():
+        download_client(version, client_jar_path)
     with zipfile.ZipFile(str(client_jar_path)) as client_jar:
         client_jar.extractall('build/clientjar')
     client = pathlib.Path('build/clientjar')
